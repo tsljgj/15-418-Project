8da65bf26e846d67b824cb9d550feb0240494c9a
Daniel Alejandro Rodriguez <darodri2@ghc56.ghc.andrew.cmu.edu>
2025-04-25 22:03:56 -0400

    Parallel changes

--- src/louvain_parallel.h
+++ src/louvain_parallel.h
@@ -1,32 +1,18 @@
 #ifndef LOUVAIN_PARALLEL_H
 #define LOUVAIN_PARALLEL_H
 
-#include "louvain_seq.h"
-#include <omp.h>
+#include "graph.h"
 #include <vector>
 #include <unordered_map>
+#include "hierarchy.h"
 
-// Structure to hold graph partitions for parallel processing
+
+// Partition descriptor (though we won't copy adjacency in this simple version)
 struct GraphPartition {
-    std::vector<int> nodeIds;                 // Original node IDs in this partition
-    std::vector<std::vector<Edge>> localAdj;  // Local adjacency list
-    std::vector<double> localDegree;          // Local degree vector
-    std::unordered_map<int, int> globalToLocal; // Maps global node ID to local index
-    std::unordered_map<int, int> localToGlobal; // Maps local index to global node ID
+    std::vector<int> nodeIds;
 };
 
-// Function to partition a graph into numPartitions parts
-std::vector<GraphPartition> partitionGraph(const Graph &g, int numPartitions);
-
-// Aggregates graph g given a partition.
-// Creates a new graph where each node corresponds to a community from "partition".
-Graph aggregateGraph(const Graph &g, const std::vector<int> &partition, std::unordered_map<int,int> &commMap);
-
-// Runs local move phase on a partition and returns updated community assignments
-std::vector<int> parallelLocalMovePhase(const Graph &g, const GraphPartition &partition, 
-                                       const std::vector<int> &initialCommunities);
-
-// Parallel implementation of the hierarchical Louvain algorithm
-void louvainParallel(const Graph &g, Hierarchy &hierarchy, int numThreads);
+// The parallel Louvain entry point:
+void louvainParallel(const Graph &g, Hierarchy &H, int numThreads);
 
 #endif // LOUVAIN_PARALLEL_H
